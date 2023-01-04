a66b53b774ad72ba6c45d42841ceca3e055aa514
Samantha Doran <samanthadoran3@gmail.com>
2023-01-03 19:32:03 -0500

    Fix build.rs to use new img folder.

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ fn main() -> io::Result<()> {
     #[cfg(windows)] {
         WindowsResource::new()
             // This path can be absolute, or relative to your crate root.
-            .set_icon("src\\img\\icon.ico")
+            .set_icon("img\\icon.ico")
             .compile()?;
     }
     Ok(())
