285113fa67d6e9c6cf7ceb2a88fc941be1455a90
chastise <equalibrium@gmail.com>
2023-01-03 20:07:23 -0800

    Let's not default our paths to windows style.

--- build.rs
+++ build.rs
@@ -4,9 +4,9 @@ use std::io;
 fn main() -> io::Result<()> {
     #[cfg(windows)] {
         WindowsResource::new()
-            // This path can be absolute, or relative to your crate root.
-            .set_icon("img\\icon.ico")
+            .set_icon("img/icon.ico")
             .compile()?;
     }
+    
     Ok(())
 }
